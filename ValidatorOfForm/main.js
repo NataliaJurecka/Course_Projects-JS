17ac3e1aca976ed1c87260b4e298f3b1efb3e85c
NataliaJ <jurecka.natalia22@gmail.com>
2023-11-06 12:40:47 +0100

    third project - work on errors & form checking

--- ValidatorOfForm/main.js
+++ ValidatorOfForm/main.js
@@ -1,15 +1,43 @@
 const username = document.querySelector('#username')
 const pass = document.querySelector('#password')
-const pass2 = document.querySelector('#passwor2')
+const pass2 = document.querySelector('#password2')
 const email = document.querySelector('#email')
 const sendBtn = document.querySelector('.send')
 const clearBtn = document.querySelector('.clear')
 const popup = document.querySelector('.popup')
 
- clearBtn.addEventListener('click', e => {
-    e.preventDefault()
+const showError = (input, msg) => {
+	const formBox = input.parentElement
+	const errMsg = formBox.querySelector('.error-text')
 
-    [username, pass, pass2, email].forEach(el => {
-        el.value = ''
-    })
- })
+	formBox.classList.add('error')
+	errMsg.textContent = msg
+}
+
+const clearError = input => {
+	const formBox = input.parentElement
+	formBox.classList.remove('error')
+}
+
+const checkForm = input => {
+	input.forEach(el => {
+		if (el.value === '') {
+			showError(el, el.placeholder)
+		} else {
+			clearError(el)
+		}
+	})
+}
+
+sendBtn.addEventListener('click', e => {
+	e.preventDefault();
+	checkForm([username, pass, pass2, email])
+})
+
+clearBtn.addEventListener('click', e => {
+	e.preventDefault();
+	[username, pass, pass2, email].forEach(el => {
+		el.value = ''
+        clearError(el)
+	})
+})
