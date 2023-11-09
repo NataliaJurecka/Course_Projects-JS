0c0f94b88abf4e9d6fe742c6e7821deedf714535
NataliaJ <jurecka.natalia22@gmail.com>
2023-11-09 20:57:19 +0100

    seventh project - js failed by now paid API subs.

---
+++ ExchangeRateApp/main.js
@@ -0,0 +1,3 @@
+/*
+JS isn't done because I had a lot of problems with both od the API's represendet and recommended in this project. As the menthor said both of them were updated and now need payed subscription. I still tried to understand how teh API works and read some documentation. 
+*/
